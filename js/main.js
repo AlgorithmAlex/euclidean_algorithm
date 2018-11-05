9354960ffe8bd64cda0de0a1d0c3ae34937b8680
AlgorithmAlex <supernippi@gmail.com>
2018-11-05 21:30:03 +0100

    finish algorithm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-<script>
-    //largest common divisor
+function calc(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    var rest = 1;
+    var result = 0;
 
-    
-</script>
+    for(var i=first; rest > 0; i++){
+        rest = first % second;
+        rest == 0 ? result = second : false;
+        first = second;
+        second = rest;
+    }
+    document.getElementById("result").value = result;
+}
